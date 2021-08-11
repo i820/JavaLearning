d4b82ace05ee1af82bb97907e67c79af1738d48b
Ilshat <ilshat820@gmail.com>
2021-08-11 23:53:07 +0300

    new line of text

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("New line of text");
         System.out.println("Hello");
         System.exit(1);
     }
